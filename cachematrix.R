5d993e5d8bd4dd891f26a2053866b693c1b77c4b
Manuel Rubiano <66189618+potestatem16@users.noreply.github.com>
2020-06-25 22:10:03 -0500

    Add files via upload
    ProgrammingAssignment2-master

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,55 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+#############################################
+#    The next function can generate a 
+#    special matrix, which is the 
+#    object to take the inverse from.
+#############################################
+makeCacheMatrix <- function(x = matrix()) {
+     inverse <- NULL
+     declare_function <- function(y) {
+          x <<- y
+          inv <<- NULL
+          }
+     get_matrix <- function() x
+     set_inverse_matrix <- function(inverse) inverse <<- inverse
+     get_inverse_matrix <- function() inverse
+     list(declare_function = declare_function,
+          get_matrix = get_matrix,
+          set_inverse_matrix = set_inverse_matrix,
+          get_inverse_matrix = get_inverse_matrix)
 }
+
 
 
 ## Write a short comment describing this function
+#################################################
+#    From the function above, which generates 
+#    the special matrix object to treat, 
+#    the function below returns the inverse 
+#    matrix from the original one.
+#################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     inverse<-x$get_inverse_matrix()
+     if(!is.null(inverse)){
+          return(inverse)
+     }
+     compute<-x$get_matrix()
+     inverse<-solve(compute, ...)
+     x$set_inverse_matrix(inverse)
+     list_to_return<-list(cat("This is the inverse matrix"),inverse)
+     return(list_to_return)
+      ## Return a matrix that is the inverse of 'x'
 }
+
+#################################################
+#    Now, with the fuctions done, it's time to 
+#    test them.
+#################################################
+
+m<-makeCacheMatrix(matrix(sample(1:9), 3,3, byrow = T))
+m$get_matrix()
+cacheSolve(m)
